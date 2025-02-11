97c275a9b95ffffe45860fb961d93ed7fb2d88ad
barbaracassani <barbara@kitsch-en.net>
2025-02-11 12:48:13 +0000

    All three loops

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -11,6 +11,8 @@ import {
 } from '../state/observables.ts'
 
 const [feed$, feed] = createSignal()
+const [sleep$, rest] = createSignal()
+const [happiness$, makeHappy] = createSignal()
 
 const feedLoop$ = (animal: Animal) => {
     return merge(
@@ -25,20 +27,54 @@ const feedLoop$ = (animal: Animal) => {
     )
 }
 
+const sleepLoop$ = (animal: Animal) => {
+    return merge(
+        tick$.pipe(map(() => BASE_DECAY_PER_INTERVAL)),
+        sleep$.pipe(map(() => -BASE_REPLENISH))
+    ).pipe(
+        scan(
+            (level, change) => Math.max(0, level + change),
+            animal.initialSleepinessPercent
+        ),
+        startWith(animal.initialSleepinessPercent)
+    )
+}
+
+const happinessLoop$ = (animal: Animal) => {
+    return merge(
+        tick$.pipe(map(() => -BASE_DECAY_PER_INTERVAL)),
+        happiness$.pipe(map(() => BASE_REPLENISH))
+    ).pipe(
+        scan(
+            (level, change) => Math.min(100, level + change),
+            animal.initialHappinessPercent
+        ),
+        startWith(animal.initialHappinessPercent)
+    )
+}
+
 const [useFeedLoop] = bind(feedLoop$, 0)
+const [useSleepLoop] = bind(sleepLoop$, 0)
+const [useHappinessLoop] = bind(happinessLoop$, 0)
 
 const AnimalCard: FC<{
     animal: Animal
     removeHandler: () => void
 }> = ({ animal, removeHandler }) => {
     const hunger = useFeedLoop(animal)
+    const sleep = useSleepLoop(animal)
+    const happiness = useHappinessLoop(animal)
     const { name, species } = animal
     return (
         <div>
             {name} {species}
             <button onClick={() => removeHandler()}>Remove</button>
             Current hunger: {hunger}
+            Current sleepiness level: {sleep}
+            Current happiness level: {happiness}
             <button onClick={feed}>Feed</button>
+            <button onClick={rest}>Rest</button>
+            <button onClick={makeHappy}>Play</button>
         </div>
     )
 }
