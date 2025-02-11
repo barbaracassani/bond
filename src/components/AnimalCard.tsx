6e250b7d953fcee6d32de28c8b0af7bee8bf90d7
barbaracassani <barbara@kitsch-en.net>
2025-02-11 13:51:45 +0000

    Buggy but saving for now

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { merge } from 'rxjs'
+import { merge, Observable, tap, withLatestFrom } from 'rxjs'
 import { scan, map, startWith } from 'rxjs/operators'
 import { createSignal } from '@react-rxjs/utils'
 import { bind } from '@react-rxjs/core'
@@ -14,39 +14,71 @@ const [feed$, feed] = createSignal()
 const [sleep$, rest] = createSignal()
 const [happiness$, makeHappy] = createSignal()
 
-const feedLoop$ = (animal: Animal) => {
+const calculateFeedDecay = (animal: Animal): number => {
+    return (BASE_DECAY_PER_INTERVAL / 100) * animal.rates.hungerIncrease
+}
+const calculateRestDecay = (animal: Animal): number => {
+    return (BASE_DECAY_PER_INTERVAL / 100) * animal.rates.sleepinessIncrease
+}
+
+const calculateHappinessDecay = (
+    animal: Animal,
+    hunger: number,
+    sleep: number
+): number => {
+    const hungerFactor = hunger >= 70 ? 1.3 : 1
+    const sleepFactor = sleep >= 70 ? 1.2 : 1
+
+    return (
+        (BASE_DECAY_PER_INTERVAL / 100) *
+        animal.rates.happinessDecay *
+        hungerFactor *
+        sleepFactor
+    )
+}
+
+const feedLoop$ = (animal: Animal): Observable<number> => {
     return merge(
-        tick$.pipe(map(() => BASE_DECAY_PER_INTERVAL)),
+        tick$.pipe(map(() => calculateFeedDecay(animal))),
         feed$.pipe(map(() => -BASE_REPLENISH))
     ).pipe(
         scan(
-            (level, change) => Math.max(0, level + change),
+            (level, change) => Math.min(100, Math.max(0, level + change)),
             animal.initialHungerPercent
         ),
         startWith(animal.initialHungerPercent)
     )
 }
 
-const sleepLoop$ = (animal: Animal) => {
+const sleepLoop$ = (animal: Animal): Observable<number> => {
     return merge(
-        tick$.pipe(map(() => BASE_DECAY_PER_INTERVAL)),
+        tick$.pipe(map(() => calculateRestDecay(animal))),
         sleep$.pipe(map(() => -BASE_REPLENISH))
     ).pipe(
+        tap((a) => {
+            console.info('sleep diff', a)
+        }),
         scan(
-            (level, change) => Math.max(0, level + change),
+            (level, change) => Math.min(100, Math.max(0, level + change)),
             animal.initialSleepinessPercent
         ),
         startWith(animal.initialSleepinessPercent)
     )
 }
 
-const happinessLoop$ = (animal: Animal) => {
+const happinessLoop$ = (animal: Animal): Observable<number> => {
     return merge(
-        tick$.pipe(map(() => -BASE_DECAY_PER_INTERVAL)),
+        tick$.pipe(
+            withLatestFrom(feedLoop$(animal), sleepLoop$(animal)),
+            map(([, hunger, sleep]) => {
+                const decay = calculateHappinessDecay(animal, hunger, sleep)
+                return -decay
+            })
+        ),
         happiness$.pipe(map(() => BASE_REPLENISH))
     ).pipe(
         scan(
-            (level, change) => Math.min(100, level + change),
+            (level, change) => Math.min(100, Math.max(0, level + change)),
             animal.initialHappinessPercent
         ),
         startWith(animal.initialHappinessPercent)
@@ -69,9 +101,9 @@ const AnimalCard: FC<{
         <div>
             {name} {species}
             <button onClick={() => removeHandler()}>Remove</button>
-            Current hunger: {hunger}
-            Current sleepiness level: {sleep}
-            Current happiness level: {happiness}
+            Current hunger: {hunger.toFixed(2)}
+            Current sleepiness level: {sleep.toFixed(2)}
+            Current happiness level: {happiness.toFixed(2)}
             <button onClick={feed}>Feed</button>
             <button onClick={rest}>Rest</button>
             <button onClick={makeHappy}>Play</button>
